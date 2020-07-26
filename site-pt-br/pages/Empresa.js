3d2b8e2633f7dec9c31993372ab7f3c4eff60943
André Rodrigues (Xor) <xor2017@outlook.com>
2020-07-25 21:47:18 -0300

    Seo página home incrementado e criada a base da página sobre a empresa

--- site-pt-br/pages/Empresa.js
+++ site-pt-br/pages/Empresa.js
@@ -5,20 +5,20 @@ import { Reset } from 'styled-reset'
 import { Template } from '../Components/Template/Style' //Template
 
 
-const Home = () => {
+const Empresa = () => {
   return (
     <React.Fragment>
      <Reset />
       <TemplateGibs>
         <Head>
-          <title>Creative World</title>
+          <title>Creative World | About-Us</title>
           <link rel="icon" href="/favicon.ico" />
           <meta name="author" content="Coffee Soluções (André Rodrigues)" />
-          <meta name="description" content="Site sobre design UI/UX" />
+          <meta name="description" content="Conheça um pouco sobre a Creative World" />
           <meta name="keywords" content="UI, UX, Design, modelagem 3D, PhotoShop, Illustrator, Mockups" />
           <meta name="robots" content="index, follow" />
           <meta property="og:type" content="article" />
-          <meta property="og:title" content="Creative World" />
+          <meta property="og:title" content="Creative World - About-us" />
           <meta property="og:url" content="" />
           <meta property="og:image" content="" />
           <meta property="article:author" content="https://xordevweb.com.br" />
@@ -29,4 +29,4 @@ const Home = () => {
   );
 }
 
-export default Home;
+export default Empresa;
